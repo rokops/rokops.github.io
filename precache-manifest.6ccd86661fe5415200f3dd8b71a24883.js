58c2a3201e6ccd574440bd772ae99fdde783133c
Rauno Korhonen <zap79rok@gmail.com>
2024-06-11 02:03:05 +0300

    release-2024-06-11T02:03:05+03:00

---
+++ precache-manifest.6ccd86661fe5415200f3dd8b71a24883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13168df39f8223f8abb3d47ed524acb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4e47686e7370b8eae1",
+    "url": "/static/js/2.eb5389a7.chunk.js"
+  },
+  {
+    "revision": "506ba63e59120733d3743fc4da501cc8",
+    "url": "/static/js/2.eb5389a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed7964de7d519acf457",
+    "url": "/static/js/main.b8e2590e.chunk.js"
+  },
+  {
+    "revision": "31fe52e0bb68f83722f1",
+    "url": "/static/js/runtime-main.7094e3bb.js"
+  }
+]);
